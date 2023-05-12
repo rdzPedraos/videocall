bb30ac25c72672f81ae13f95b3effecfb1a03530
Ascecent <andres85044@gmail.com>
2023-05-11 19:28:43 -0500

    Update Naming And Resolve Broken Imports

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,29 @@
+import { useState, createContext } from 'react';
+import PropTypes from 'prop-types';
+
+const UserContext = createContext();
+
+function UserProvider({ children }) {
+	const [name, setName] = useState('Carlitos');
+
+	const onChangeName = name => {
+		setName(name);
+	};
+
+	return (
+		<UserContext.Provider
+			value={{
+				name: name,
+				setName: onChangeName,
+			}}
+		>
+			{children}
+		</UserContext.Provider>
+	);
+}
+
+UserProvider.propTypes = {
+	children: PropTypes.array,
+};
+
+export { UserProvider, UserContext };
---
+++ frontend/src/partials/ButtonsGroup.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+
+import Button from '../components/Button';
+
+import {
+	PhoneXMarkIcon,
+	MicrophoneIcon,
+	VideoCameraIcon,
+	ArrowPathIcon,
+} from '@heroicons/react/24/outline';
+
+function ButtonsGroup({ className, ...props }) {
+	return (
+		<section
+			className={`flex items-center p-5  rounded-2xl border-2 border-base-500 bg-base-500 bg-opacity-30 ${className}`}
+			{...props}
+		>
+			<div className='text-gray-500 max-w-[250px]'>
+				<p className='font-bold'>Ahora mismo estás en vivo</p>
+				<p>Asegurate de no compartir tus datos personales</p>
+			</div>
+
+			<div className='flex gap-2'>
+				<Button variant='danger'>
+					<PhoneXMarkIcon className='w-6 h-6' />
+				</Button>
+				<Button variant='success'>
+					<ArrowPathIcon className='w-6 h-6' />
+				</Button>
+				<Button>
+					<MicrophoneIcon className='w-6 h-6' />
+				</Button>
+				<Button>
+					<VideoCameraIcon className='w-6 h-6' />
+				</Button>
+			</div>
+		</section>
+	);
+}
+
+ButtonsGroup.propTypes = {
+	className: PropTypes.string,
+};
+
+ButtonsGroup.defaultProps = {
+	className: '',
+};
+
+export default ButtonsGroup;
