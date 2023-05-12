daa182dbb9efdf073552410684b110d55d57b9da
Ascecent <andres85044@gmail.com>
2023-05-11 20:17:32 -0500

    Install PostCSS And Autoprefixer For Tailwind Usage Without Terminal

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./src/**/*.jsx'],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			colors: {
