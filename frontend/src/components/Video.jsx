73a012d5bf7d24dd291d6415845c84262f6ae634
Ascecent <andres85044@gmail.com>
2023-05-11 19:39:57 -0500

    Format File Using Prettier Config

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -1,45 +1,47 @@
-import { useEffect, useRef } from "react";
-import PropTypes from "prop-types";
-import Spinner from "./Spinner";
+import { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import Spinner from './Spinner';
 
 function Video({ stream, className, ...props }) {
-    const videoRef = useRef();
+	const videoRef = useRef();
 
-    useEffect(() => {
-        if (videoRef.current && stream) {
-            videoRef.current.srcObject = stream;
-        }
-    }, [stream]);
+	useEffect(() => {
+		if (videoRef.current && stream) {
+			videoRef.current.srcObject = stream;
+		}
+	}, [stream]);
 
-    return (
-        <div className={`rounded-lg overflow-hidden bg-base-500 ${className}`}>
-            {stream ? (
-                <video
-                    className="w-full h-full object-cover"
-                    playsInline
-                    autoPlay
-                    ref={videoRef}
-                    {...props}
-                />
-            ) : (
-                <div className="w-full h-full grid place-items-center">
-                    <Spinner />
-                </div>
-            )}
-        </div>
-    );
+	return (
+		<div
+			className={`rounded-lg overflow-hidden bg-base-500 h-full ${className}`}
+		>
+			{stream ? (
+				<video
+					className='w-full h-full object-cover'
+					playsInline
+					autoPlay
+					ref={videoRef}
+					{...props}
+				/>
+			) : (
+				<div className='w-full h-full grid place-items-center'>
+					<Spinner />
+				</div>
+			)}
+		</div>
+	);
 }
 
 Video.propTypes = {
-    stream: PropTypes.object,
-    className: PropTypes.string,
+	stream: PropTypes.object,
+	className: PropTypes.string,
 };
 
 Video.defaultProps = {
-    className: "",
-    props: {
-        muted: false,
-    },
+	className: '',
+	props: {
+		muted: false,
+	},
 };
 
 export default Video;
