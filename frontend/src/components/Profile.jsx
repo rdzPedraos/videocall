fbb0ac93f4ae1b53541bc0aff705561a95303a2d
Ascecent <andres85044@gmail.com>
2023-05-11 20:21:25 -0500

    Add showName Prop And Name Rendering Based On It

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,13 +1,24 @@
 import PropTypes from 'prop-types';
 
 import Video from './Video';
-import TextEdit from './TextEdit';
 
-function Profile({ stream, name, mutedVideo, className }) {
+function Profile({ stream, name, mutedVideo, showName, className }) {
 	return (
-		<div className={className}>
+		<div className={`relative ${className}`}>
 			<Video muted={mutedVideo} stream={stream} />
-			<div>{name}</div>
+
+			{showName && (
+				<div
+					className='absolute bottom-0 w-full py-4 px-6'
+					style={{
+						background:
+							'linear-gradient(0deg, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%)',
+						borderRadius: '0 0 0.5rem 0.5rem',
+					}}
+				>
+					{name || 'Buscando...'}
+				</div>
+			)}
 		</div>
 	);
 }
@@ -19,12 +30,14 @@ Profile.propTypes = {
 	editByDefault: PropTypes.bool,
 	mutedVideo: PropTypes.bool,
 	className: PropTypes.string,
+	showName: PropTypes.bool,
 };
 
 Profile.defaultProps = {
 	editByDefault: false,
 	mutedVideo: false,
 	className: '',
+	showName: false,
 };
 
 export default Profile;
