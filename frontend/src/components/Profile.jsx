9233eea33592924bb233c4c0d616651fde700689
Ascecent <andres85044@gmail.com>
2023-05-11 19:39:45 -0500

    Format File Using Prettier Config

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,30 +1,30 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import Video from "./Video";
-import TextEdit from "./TextEdit";
+import Video from './Video';
+import TextEdit from './TextEdit';
 
 function Profile({ stream, name, mutedVideo, className }) {
-    return (
-        <div className={className}>
-            <Video muted={mutedVideo} stream={stream} />
-            <div>{name}</div>
-        </div>
-    );
+	return (
+		<div className={className}>
+			<Video muted={mutedVideo} stream={stream} />
+			<div>{name}</div>
+		</div>
+	);
 }
 
 Profile.propTypes = {
-    stream: PropTypes.object,
-    name: PropTypes.string.isRequired,
-    onChangeName: PropTypes.func,
-    editByDefault: PropTypes.bool,
-    mutedVideo: PropTypes.bool,
-    className: PropTypes.string,
+	stream: PropTypes.object,
+	name: PropTypes.string.isRequired,
+	onChangeName: PropTypes.func,
+	editByDefault: PropTypes.bool,
+	mutedVideo: PropTypes.bool,
+	className: PropTypes.string,
 };
 
 Profile.defaultProps = {
-    editByDefault: false,
-    mutedVideo: false,
-    className: "",
+	editByDefault: false,
+	mutedVideo: false,
+	className: '',
 };
 
 export default Profile;
