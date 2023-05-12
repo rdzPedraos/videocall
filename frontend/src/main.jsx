e775e8bcf5ebc3ce5bddcb386cac373542079948
Ascecent <andres85044@gmail.com>
2023-05-11 20:18:24 -0500

    Update Styles Import And Format File

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
-import ReactDOM from "react-dom/client";
-import "./../public/styles.css";
+import ReactDOM from 'react-dom/client';
 
-import App from "./App.jsx";
-import Layout from "./layouts/Layout";
+import App from './App.jsx';
+import Layout from './layouts/Layout';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <Layout>
-        <App />
-    </Layout>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Layout>
+		<App />
+	</Layout>
 );
