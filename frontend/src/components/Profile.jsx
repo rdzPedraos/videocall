4da1fd42e841611e8e443e3e3cef7ec782eb9ea7
rdzPedraos <juancursosrodriguez@gmail.com>
2023-05-11 21:38:51 -0500

    deleted parameters in Profile Component

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,43 +1,41 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import Video from './Video';
+import Video from "./Video";
 
 function Profile({ stream, name, mutedVideo, showName, className }) {
-	return (
-		<div className={`relative ${className}`}>
-			<Video muted={mutedVideo} stream={stream} />
+    return (
+        <div className={`relative ${className}`}>
+            <Video muted={mutedVideo} stream={stream} />
 
-			{showName && (
-				<div
-					className='absolute bottom-0 w-full py-4 px-6'
-					style={{
-						background:
-							'linear-gradient(0deg, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%)',
-						borderRadius: '0 0 0.5rem 0.5rem',
-					}}
-				>
-					{name || 'Buscando...'}
-				</div>
-			)}
-		</div>
-	);
+            {showName && (
+                <div
+                    className="absolute bottom-0 w-full py-4 px-6"
+                    style={{
+                        background:
+                            "linear-gradient(0deg, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%)",
+                        borderRadius: "0 0 0.5rem 0.5rem",
+                    }}
+                >
+                    {name || "Buscando..."}
+                </div>
+            )}
+        </div>
+    );
 }
 
 Profile.propTypes = {
-	stream: PropTypes.object,
-	name: PropTypes.string.isRequired,
-	onChangeName: PropTypes.func,
-	editByDefault: PropTypes.bool,
-	mutedVideo: PropTypes.bool,
-	className: PropTypes.string,
-	showName: PropTypes.bool,
+    stream: PropTypes.object,
+    name: PropTypes.string.isRequired,
+    mutedVideo: PropTypes.bool,
+    showName: PropTypes.bool,
+    className: PropTypes.string,
 };
 
 Profile.defaultProps = {
-	editByDefault: false,
-	mutedVideo: false,
-	className: '',
-	showName: false,
+    editByDefault: false,
+    mutedVideo: false,
+    className: "",
+    showName: false,
 };
 
 export default Profile;
